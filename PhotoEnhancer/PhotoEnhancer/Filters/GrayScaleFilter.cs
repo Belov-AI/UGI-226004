e5e741fb3d45965dc9826883d952d0edb6777fa2
Belov-AI <Belov.Alexander@urfu.ru>
2024-03-12 18:33:45 +0500

    Добавлен фильтр "Оттенки серого" Добравлен абстрактный класс PixelFilter

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/GrayScaleFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class GrayScaleFilter : PixelFilter
+    {
+        public override ParameterInfo[] GetParametersInfo()
+        {
+            return new ParameterInfo[0];
+        }
+
+        public override Pixel ProcessPixel(Pixel p, double[] parameters)
+        {
+            var lightness = 0.3 * p.R + 0.6 * p.G + 0.1 * p.B;
+            return new Pixel(lightness, lightness, lightness);
+        }
+
+        public override string ToString()
+        {
+            return "Оттенки серого";
+        }
+    }
+}
