75f8f42a9f52879d3ec8fe231288e7bff87db544
Belov-AI <Belov.Alexander@urfu.ru>
2024-03-19 19:26:39 +0500

    Обобщенный метод

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var s = new List<string> (("H e l l o").Split());
+
+            PrintArray(numbers);
+            PrintArray(s.ToArray());
+
+            Console.ReadKey();
+        }
+
+        static void PrintArray<T>(T[] array)
+        {
+            foreach(T elem in array)
+                Console.WriteLine(elem.ToString() + " ");
+        }
+    }
+}
