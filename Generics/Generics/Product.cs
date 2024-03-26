ad27bd98a9395848660a2e3ca1924e2854380b6e
Belov-AI <Belov.Alexander@urfu.ru>
2024-03-26 17:44:28 +0500

    Использование обощенных методов, классов, интерфейсов

---
+++ Generics/Generics/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Product<TPrice> : IComparable<Product<TPrice>>
+        where TPrice : IComparable<TPrice>
+    {
+        public string Name { get; set; }
+        public TPrice Price {  get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public Product(string name, TPrice price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Product(string name) : this(name, default(TPrice)) { }
+
+        public Product() : this("no name") { }
+
+        public void Sell()
+        {
+            Console.WriteLine($"Вы продали {Name} за {Price} руб.");
+        }
+
+        public int CompareTo(Product<TPrice> other)
+        {
+            return Price.CompareTo( other.Price );
+        }
+    }
+}
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -12,18 +12,45 @@ namespace Generics
         {
             var numbers = new[] { 1, 2, 3, 4, 5, 6 };
 
-            var s = new List<string> (("H e l l o").Split());
+            char[] s;
 
+            var word = "Hello";
+
+            s = new char[word.Length];
+            for (int i = 0; i < s.Length; i++)
+                s[i] = word[i];
+
+            var bread = new Product<int> { Name = "хлеб", Price = 37 };
+            var milk = new Product<double> { Name = "молоко", Price = 30.5 };
+            var cheese = new Product<string> { Name = "сыр", Price = "0 (нет в наличии)" };
+            var candies = new Product<double> { Name = "конфеты", Price = 261.7 };
+            var cake = new Product<double> { Name = "пироженное", Price = 20.3 };
+
+            var goods = new object[] { bread, milk, cheese };
+            
             PrintArray(numbers);
-            PrintArray(s.ToArray());
+            PrintArray(s);
+            PrintArray(goods);
+
+            bread.Sell();
+            milk.Sell();
+            cheese.Sell();
+
+            var set = new Product<double>[] { milk, candies, cake };
+
+            PrintArray(set);
+            Array.Sort(set);
+            PrintArray(set);
 
             Console.ReadKey();
         }
 
         static void PrintArray<T>(T[] array)
         {
-            foreach(T elem in array)
-                Console.WriteLine(elem.ToString() + " ");
+            foreach(var elem in array)
+                Console.Write(elem.ToString() + " ");
+
+            Console.WriteLine();
         }
     }
 }
